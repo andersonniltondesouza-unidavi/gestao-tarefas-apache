f91a38684a8972c0cb1242f908208dc20406ac60
andersonniltondesouza-unidavi <anderson.souza@unidavi.edu.br>
2026-06-14 13:16:39 -0300

    defesa: adiciona submenu de processos na opcao 6 do menu

--- scripts/menu.sh
+++ scripts/menu.sh
@@ -40,7 +40,47 @@ while true; do
         3)  ./03_estrutura.sh; aguardar_usuario ;;
         4)  ./04_backup.sh; aguardar_usuario ;;
         5)  ./05_deploy.sh; aguardar_usuario ;;
-        6)  ./06_processos.sh listar; aguardar_usuario ;;
+        6)
+            while true; do
+                clear
+                echo "=========================================================="
+                echo "          SUBMENU - GERENCIAMENTO DE PROCESSOS"
+                echo "=========================================================="
+                echo " [1] Listar processos ativos"
+                echo " [2] Buscar processo por nome"
+                echo " [3] Matar processo por PID"
+                echo " [0] Voltar ao menu principal"
+                echo "=========================================================="
+                echo -n "Escolha uma operacao: "
+                read -r OP_SUB
+                
+                case "$OP_SUB" in
+                    1)
+                        ./06_processos.sh listar
+                        aguardar_usuario
+                        ;;
+                    2)
+                        echo -n "Digite o nome do processo: "
+                        read -r NOME_BUSCA
+                        ./06_processos.sh buscar "$NOME_BUSCA"
+                        aguardar_usuario
+                        ;;
+                    3)
+                        echo -n "Digite o PID do processo: "
+                        read -r PID_MATAR
+                        ./06_processos.sh matar "$PID_MATAR"
+                        aguardar_usuario
+                        ;;
+                    0)
+                        break
+                        ;;
+                    *)
+                        echo -e "\n[ERRO] Opcao invalida!"
+                        sleep 1.5
+                        ;;
+                esac
+            done
+            ;;
         7)  ./07_monitoramento.sh; aguardar_usuario ;;
         8)  ./08_usuarios_permissoes.sh; aguardar_usuario ;;
         9)  ./09_relatorio.sh; aguardar_usuario ;;
